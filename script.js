a9bca038a64690a14d73c0468d54e2f898fd9b24
tigerTegenrot <96125810+tigerTegenrot@users.noreply.github.com>
2022-02-10 17:46:14 +0100

    Got arduino joystick working

--- script.js
+++ script.js
@@ -4,6 +4,64 @@ const next_button = document.getElementById("next-button");
 const prev_button = document.getElementById("prev-button");
 const pokeBtn = document.getElementById("poke-card");
 
+// Fetch the Arduino
+
+const getXAxis = () => {
+    state.joystick.x;
+    state.joystick.pressed;
+  }
+
+const requestPortButton = document.querySelector("#request-port-access");
+requestPortButton.addEventListener("pointerdown", async (event) => {
+  // First, request port access, which hopefully leads to a connection
+  const port = await navigator.serial.requestPort(); 
+  state.serial = port;
+
+  await state.serial.open({ baudRate: 9600 }); 
+  
+  await readJSONFromArduino("joystick", getXAxis);
+});
+
+const readJSONFromArduino = async (propertyName, callback) => {
+    if (!state.serial) throw new Error("No Arduino connected to read the data from!");
+  
+    const textDecoder = new TextDecoderStream();
+    const readableStreamClosed = state.serial.readable.pipeTo(textDecoder.writable);
+    const reader = textDecoder.readable.getReader();
+    let lineBuffer = "";
+  
+    // Listen to data coming from the serial device.
+    while (true) {
+      const response = await reader.read();
+  
+      if (response.done) {
+        reader.releaseLock();
+        break;
+      }
+  
+      lineBuffer += response.value;
+      const lines = lineBuffer.split("\n");
+      if (lines.length > 1) {
+        lineBuffer = lines.pop();
+        const line = lines.pop().trim();
+        const json = JSON.parse(line);
+        state[propertyName] = json;
+        callback(json);
+        movePkm();
+        morePkm();
+      }
+    }
+    
+  }
+
+const state = {
+    serial: null,
+    joystick: {
+      x: 0,
+      pressed: false,
+    }
+  };
+// Fetch the Pokemon API
 const fetchPokemon = () => {
     let promises = [];
     const url = `https://pokeapi.co/api/v2/pokemon/${num}`;
@@ -70,30 +128,38 @@ pokeBtn.addEventListener("click", () => {
     fetchMore();
 })
 
-const x = 500;
+let haveMoved = false;
+
+const addButton = document.querySelector("#add-button")
+
+
+const morePkm =  () => {
+    if(state.joystick.pressed === true){
+        fetchMore();
+    }
+}
 
 const movePkm = () => {
-    if (x < 200) {
+    if (state.joystick.x < 8) {
         if (!haveMoved) {
             num++;
+            fetchPokemon();
             haveMoved = true;
-        }
-    } else if (x > 1000) {
+    }
+    } else if (state.joystick.x > 1020) {
         if (!haveMoved) {
             num--;
+            fetchPokemon();
             haveMoved = true;
         }
     } else {
         haveMoved = false;
     }
 }
-const addButton = document.querySelector("#add-button")
 
-addButton.addEventListener("pointerdown", (event) => {
-    x = 100;
-})
 
-movePkm(fetchPokemon);
+fetchPokemon();
+
 /*
 next_button.addEventListener("click", () => {
     if (num < 898) {
@@ -110,7 +176,7 @@ prev_button.addEventListener("click", () => {
     fetchPokemon();
 })
 */
-fetchPokemon();
+
 
 
 
