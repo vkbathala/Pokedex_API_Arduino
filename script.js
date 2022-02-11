3435ac7c1b9c36c31fe43e60306f911e6d259b19
tigerTegenrot <96125810+tigerTegenrot@users.noreply.github.com>
2022-02-11 13:25:53 +0100

    Arduino LED

--- script.js
+++ script.js
@@ -9,59 +9,80 @@ const pokeBtn = document.getElementById("poke-card");
 const getXAxis = () => {
     state.joystick.x;
     state.joystick.pressed;
-  }
+}
 
 const requestPortButton = document.querySelector("#request-port-access");
 requestPortButton.addEventListener("pointerdown", async (event) => {
-  // First, request port access, which hopefully leads to a connection
-  const port = await navigator.serial.requestPort(); 
-  state.serial = port;
+    // First, request port access, which hopefully leads to a connection
+    const port = await navigator.serial.requestPort();
+    state.serial = port;
 
-  await state.serial.open({ baudRate: 9600 }); 
-  
-  await readJSONFromArduino("joystick", getXAxis);
+    await state.serial.open({ baudRate: 9600 });
+
+    await readJSONFromArduino("joystick", getXAxis);
+
 });
+
+/*
+    readJSONFromArduino("joystick", async () => {
+        getXAxis();
+        setLEDToPkmWeight();
+    });
+*/
 
 const readJSONFromArduino = async (propertyName, callback) => {
     if (!state.serial) throw new Error("No Arduino connected to read the data from!");
-  
+
     const textDecoder = new TextDecoderStream();
     const readableStreamClosed = state.serial.readable.pipeTo(textDecoder.writable);
     const reader = textDecoder.readable.getReader();
     let lineBuffer = "";
-  
+
     // Listen to data coming from the serial device.
     while (true) {
-      const response = await reader.read();
-  
-      if (response.done) {
-        reader.releaseLock();
-        break;
-      }
-  
-      lineBuffer += response.value;
-      const lines = lineBuffer.split("\n");
-      if (lines.length > 1) {
-        lineBuffer = lines.pop();
-        const line = lines.pop().trim();
-        const json = JSON.parse(line);
-        state[propertyName] = json;
-        callback(json);
-        movePkm();
-        morePkm();
-      }
+        const response = await reader.read();
+
+        if (response.done) {
+            reader.releaseLock();
+            break;
+        }
+
+        lineBuffer += response.value;
+        const lines = lineBuffer.split("\n");
+        if (lines.length > 1) {
+            lineBuffer = lines.pop();
+            const line = lines.pop().trim();
+            const json = JSON.parse(line);
+            state[propertyName] = json;
+            callback(json);
+            movePkm();
+            morePkm();
+        }
     }
-    
-  }
+
+}
 
 const state = {
+    dataToWrite: {
+      brightness: 0,
+    },
     serial: null,
     joystick: {
       x: 0,
       pressed: false,
-    }
-  };
+    },
+    writing: false,
+  }
+
 // Fetch the Pokemon API
+
+const pkmWeight = async () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${num}`).then((response) => response.json())
+    .then((result) => {
+        setLEDToPkmWeight(result.weight);
+    });
+}
+
 const fetchPokemon = () => {
     let promises = [];
     const url = `https://pokeapi.co/api/v2/pokemon/${num}`;
@@ -74,8 +95,10 @@ const fetchPokemon = () => {
             id: result.id
         }));
         displayPokemon(pokemon);
+        pkmWeight();
     });
 };
+
 
 const fetchMore = () => {
     let promises = [];
@@ -89,9 +112,13 @@ const fetchMore = () => {
             id: result.id,
             weight: result.weight
         }));
+        
         displayMore(pokemon);
+        
     });
 }
+
+
 
 const displayPokemon = (pokemon) => {
     const pokemonHTMLString = pokemon
@@ -106,6 +133,7 @@ const displayPokemon = (pokemon) => {
         )
         .join('');
     pokedex.innerHTML = pokemonHTMLString;
+
 };
 
 const displayMore = (pokemon) => {
@@ -122,6 +150,8 @@ const displayMore = (pokemon) => {
         )
         .join('');
     pokedex.innerHTML = pokemonHTMLString;
+    
+
 }
 
 pokeBtn.addEventListener("click", () => {
@@ -133,8 +163,8 @@ let haveMoved = false;
 const addButton = document.querySelector("#add-button")
 
 
-const morePkm =  () => {
-    if(state.joystick.pressed === true){
+const morePkm = () => {
+    if (state.joystick.pressed === true) {
         fetchMore();
     }
 }
@@ -145,7 +175,7 @@ const movePkm = () => {
             num++;
             fetchPokemon();
             haveMoved = true;
-    }
+        }
     } else if (state.joystick.x > 1020) {
         if (!haveMoved) {
             num--;
@@ -157,6 +187,41 @@ const movePkm = () => {
     }
 }
 
+// LED on pkm weight
+const writeJSONToArduino = async (propertyName, callback) => {
+    if (!state.serial) throw new Error("No Arduino connected to write the data to!");
+    if (state.writing) return; // it is possible to send double writes, which we do not want!
+
+    state.writing = true;
+    const data = state[propertyName]; // First, we get the object an object and turn it into JSON.
+    const json = JSON.stringify(data, null, 0); // Transform our internal JS object into JSON representation, which we store as a string
+
+    // The serial writer will want the data in a specific format, which we can do with the TextEncoder object, see https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder
+    const payload = new TextEncoder().encode(json);
+
+    // Get the writer, write to it and then release it for the next write
+    const writer = await state.serial.writable.getWriter();
+    await writer.write(payload);
+    writer.releaseLock();
+
+    if (callback) callback(); // Run the callback function, if any
+    state.writing = false;
+} // <-----
+
+const mapRange = (value, fromLow, fromHigh, toLow, toHigh) => {
+    return toLow + (toHigh - toLow) * (value - fromLow) / (fromHigh - fromLow);
+} // <----
+
+const setLEDToPkmWeight = async (weight) => {
+    state.dataToWrite.brightness = mapRange(weight, 0, 1500, 0, 255);
+    writeJSONToArduino("dataToWrite");
+    console.log(weight)
+} // <-----
+
+
+
+
+// fetch pokemons
 
 fetchPokemon();
 
