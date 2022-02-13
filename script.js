1042ab43bfe9271db1aeb06e9711bca5c9ae75de
vkbathala <69809715+vkbathala@users.noreply.github.com>
2022-02-13 23:06:04 +0100

    commented JS code

--- script.js
+++ script.js
@@ -1,64 +1,54 @@
-const pokedex = document.getElementById('pokedex');
-let num = 1;
-const next_button = document.getElementById("next-button");
-const prev_button = document.getElementById("prev-button");
-const pokeBtn = document.getElementById("poke-card");
+const pokedex = document.getElementById('pokedex'); // The html list element containing the pokemon
+let num = 1; // variable that keeps track of the pokemon number we are on
+const pokeBtn = document.getElementById("poke-card"); // creates a variable for the card containing the pkmn
 
 // Fetch the Arduino
 
 const getXAxis = () => {
-    state.joystick.x;
-    state.joystick.pressed;
+    state.joystick.x; // deals with the joystick'c current x-axis orientation
+    state.joystick.pressed; // deals with joystick's current pressed/not pressed status
 }
 
-const requestPortButton = document.querySelector("#request-port-access");
-requestPortButton.addEventListener("pointerdown", async (event) => {
+const requestPortButton = document.querySelector("#request-port-access"); // Variable for our request port access button
+requestPortButton.addEventListener("pointerdown", async (event) => { //checks if request port button pressed
     // First, request port access, which hopefully leads to a connection
-    const port = await navigator.serial.requestPort();
+    const port = await navigator.serial.requestPort(); // requests port access
     state.serial = port;
 
-    await state.serial.open({ baudRate: 9600 });
+    await state.serial.open({ baudRate: 9600 }); // determines speed of communication over data channel
 
-    await readJSONFromArduino("joystick", async (json) => {
-        movePkm();
-        morePkm();
-        writeJSONToArduino("dataToWrite");
+    await readJSONFromArduino("joystick", async (json) => { // readJSONFromArduino method
+        movePkm(); // calls movePkm method
+        morePkm(); //calls morePkm method
+        writeJSONToArduino("dataToWrite"); // calls writeJSONToArduino method
     });
 
 });
 
-/*
-    readJSONFromArduino("joystick", async () => {
-        getXAxis();
-        setLEDToPkmWeight();
-    });
-*/
+const readJSONFromArduino = async (propertyName, callback) => { // readJSONFromArduino method
+    if (!state.serial) throw new Error("No Arduino connected to read the data from!"); // throws error if no Arduino connected
 
-const readJSONFromArduino = async (propertyName, callback) => {
-    if (!state.serial) throw new Error("No Arduino connected to read the data from!");
-
-    const textDecoder = new TextDecoderStream();
-    const readableStreamClosed = state.serial.readable.pipeTo(textDecoder.writable);
-    const reader = textDecoder.readable.getReader();
-    let lineBuffer = "";
+    const textDecoder = new TextDecoderStream(); // TextDecoderStream converts a stream of strings into bytes in the UTF-8 encoding
+    const reader = textDecoder.readable.getReader(); // instantiates a reader for the textDecoder
+    let lineBuffer = ""; // simple line buffer
 
     // Listen to data coming from the serial device.
-    while (true) {
-        const response = await reader.read();
+    while (true) { // while true
+        const response = await reader.read(); // response is assigned the value of the reader.read
 
-        if (response.done) {
-            reader.releaseLock();
-            break;
+        if (response.done) { // if the response is done
+            reader.releaseLock(); // release it
+            break; // exit the while loop
         }
 
-        lineBuffer += response.value;
-        const lines = lineBuffer.split("\n");
-        if (lines.length > 1) {
-            lineBuffer = lines.pop();
-            const line = lines.pop().trim();
-            const json = JSON.parse(line);
-            state[propertyName] = json;
-            callback(json);
+        lineBuffer += response.value; // response.value is added to the lineBuffer
+        const lines = lineBuffer.split("\n"); // \n creates a new line
+        if (lines.length > 1) { // if the line length is greater than 1
+            lineBuffer = lines.pop(); //removes last element
+            const line = lines.pop().trim(); // removes whitespace
+            const json = JSON.parse(line); // parses a json string
+            state[propertyName] = json; // the value of the array portion is set to the json
+            callback(json); // calls callback method
 
         }
     }
@@ -67,69 +57,75 @@ const readJSONFromArduino = async (propertyName, callback) => {
 
 
 
-const state = {
+const state = { // instantiates state
     dataToWrite: {
-      brightness: 0,
+      brightness: 0, // sets brightness to zero
     },
-    serial: null,
+    serial: null, // sets serial to null
     joystick: {
-      x: 0,
-      pressed: false,
+      x: 0, //sets x value to 0
+      pressed: false, //sets pressed status to false
     },
-    writing: false,
+    writing: false, // sets writing to false
   }
 
 // Fetch the Pokemon API
 
-const pkmWeight = async () => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${num}`).then((response) => response.json())
+const pkmWeight = async () => { // creates an asynchronous method called pkmWeight
+    fetch(`https://pokeapi.co/api/v2/pokemon/${num}`).then((response) => response.json()) //fetches json from the url
     .then((result) => {
-        setLEDToPkmWeight(result.weight);
+        setLEDToPkmWeight(result.weight); // calls setLEDtoPkmWeight method with the the weight data from result json
     });
 }
 
-const fetchPokemon = () => {
-    let promises = [];
-    const url = `https://pokeapi.co/api/v2/pokemon/${num}`;
-    promises.push(fetch(url).then((res) => res.json()));
-    Promise.all(promises).then((results) => {
-        let pokemon = results.map((result) => ({
-            name: result.name,
-            image: result.sprites['front_default'],
-            type: result.types.map((type) => type.type.name).join(', '),
-            id: result.id
+const fetchPokemon = () => { // instantiates new method called fetchPokemon
+    let promises = []; // instantiates array
+    const url = `https://pokeapi.co/api/v2/pokemon/${num}`; // accesses the url for the specific pokemon number
+    promises.push(fetch(url).then((res) => res.json())); // fetches json data from url
+    Promise.all(promises).then((results) => { // takes an iterable of promises as an input, and returns a
+                                                        // single Promise that resolves to an array of the results of
+                                                        // the input promises
+        let pokemon = results.map((result) => ({ //maps data from results
+            name: result.name, // name variable to result.name
+            image: result.sprites['front_default'], // image variable to result.sprites
+            type: result.types.map((type) => type.type.name).join(', '), // type variable to result.types
+            id: result.id // id variable to result.id
         }));
-        displayPokemon(pokemon);
-        pkmWeight();
+        displayPokemon(pokemon); // calls displayPokemon method
+        pkmWeight(); // calls pkmWeight function
     });
 };
 
 
-const fetchMore = () => {
-    let promises = [];
-    const url = `https://pokeapi.co/api/v2/pokemon/${num}`;
-    promises.push(fetch(url).then((res) => res.json()));
-    Promise.all(promises).then((results) => {
-        let pokemon = results.map((result) => ({
-            name: result.name,
-            image: result.sprites['front_default'],
-            type: result.types.map((type) => type.type.name).join(', '),
-            id: result.id,
-            weight: result.weight,
-            height: result.height,
-            base_experience: result.base_experience
+const fetchMore = () => { // instantiates new method called fetchMore
+    let promises = []; // instantiates array
+    const url = `https://pokeapi.co/api/v2/pokemon/${num}`; // accesses the url for the specific pokemon number
+    promises.push(fetch(url).then((res) => res.json())); // fetches json data from url
+    Promise.all(promises).then((results) => { // takes an iterable of promises as an input, and returns a
+                                                        // single Promise that resolves to an array of the results of
+                                                        // the input promises
+        let pokemon = results.map((result) => ({ //maps data from results
+            name: result.name, //maps data from results
+            image: result.sprites['front_default'], // image variable to result.sprites
+            type: result.types.map((type) => type.type.name).join(', '), // type variable to result.types
+            id: result.id, // id variable to result.id
+            weight: result.weight, // weight variable to result.weight
+            height: result.height, // height variable to result.height
+            base_experience: result.base_experience // base_experience variable to result.base_experience
         }));
         
-        displayMore(pokemon);
-        pkmWeight();
+        displayMore(pokemon); // calls displayMore method
+        pkmWeight(); // calls pkmWeight method
     });
 }
 
 
 
-const displayPokemon = (pokemon) => {
-    const pokemonHTMLString = pokemon
-        .map(
+const displayPokemon = (pokemon) => { // creates method displayPokemon with parameter 'pokemon'
+    const pokemonHTMLString = pokemon // instantiates pokemonHTMLString
+        .map( // creates a new array to populate with info
+        // I dont know how to comment the next 10 lines line by line, but it basically makes a card with info about the
+        // pokemon including its image, id, name, and typing.
             (pokemon) => `
         <li class="card">
             <img class="card-image" src="${pokemon.image}"/>
@@ -139,15 +135,17 @@ const displayPokemon = (pokemon) => {
     `
         )
         .join('');
-    pokedex.innerHTML = pokemonHTMLString;
+    pokedex.innerHTML = pokemonHTMLString; // sets the HTML markup contained within the element.
 
 };
 
-const displayMore = (pokemon) => {
-    const pokemonHTMLString = pokemon
-        .map(
-            (pokemon) => `
-        <li class="card">
+const displayMore = (pokemon) => { // creates method displayMore with parameter 'pokemon'
+    const pokemonHTMLString = pokemon // instantiates pokemonHTMLString
+        .map( // creates a new array to populate with info
+        // I dont know how to comment the next 10 lines line by line, but it basically makes a card with info about the
+        // pokemon including its image, id, name, typing, weight, height, and base experience.
+            (pokemon) => `          
+        <li class="card"> 
             <img class="card-image" src="${pokemon.image}"/>
             <h2 class="card-title">#${pokemon.id} ${pokemon.name}</h2>
             <p class="card-subtitle">Type: ${pokemon.type}</p>
@@ -159,115 +157,67 @@ const displayMore = (pokemon) => {
     `
         )
         .join('');
-    pokedex.innerHTML = pokemonHTMLString;
+    pokedex.innerHTML = pokemonHTMLString; // sets the HTML markup contained within the element.
     
 
 }
 
-pokeBtn.addEventListener("click", () => {
-    fetchMore();
+pokeBtn.addEventListener("click", () => { // Event listener for pokeBtn that activates if it is clicked
+    fetchMore(); // calls fetchMore method
 })
 
-let haveMoved = false;
+let haveMoved = false; // instantiates haveMoved variable to false
 
-const addButton = document.querySelector("#add-button")
+const addButton = document.querySelector("#add-button") // creates a variable for the div containing add-button
 
 
-const morePkm = () => {
-    if (state.joystick.pressed === true) {
-        fetchMore();
+const morePkm = () => { // / instantiates morePkmn method
+    if (state.joystick.pressed === true) { // if the joystick is pressed
+        fetchMore(); // call the fetchMore method
     }
 }
 
-const movePkm = () => {
-    if (state.joystick.x < 8) {
-        if (!haveMoved) {
-            num++;
-            fetchPokemon();
-            haveMoved = true;
+const movePkm = () => { // instantiates movePkmn method
+    if (state.joystick.x < 8) { // if joystick x-axis less than 8
+        if (!haveMoved) { // and if haveMoved is false
+            num++; // the number that keeps track of pmkn num increases by 1
+            fetchPokemon(); // the fetch pokemon method is called to bring up the pkmn corresponding w/ new number
+            haveMoved = true; // haveMoved becomes true
         }
-    } else if (state.joystick.x > 1020) {
-        if (!haveMoved) {
-            num--;
-            fetchPokemon();
-            haveMoved = true;
+    } else if (state.joystick.x > 1020) { // else if  the joystick x axis is greater than 1020
+        if (!haveMoved) { // and if haveMoved is false
+            num--; // the number that keeps track of pmkn num decreases by 1
+            fetchPokemon(); // the fetch pokemon method is called to bring up the pkmn corresponding w/ new number
+            haveMoved = true; // haveMoved becomes true
         }
-    } else {
-        haveMoved = false;
+    } else { // otherwise
+        haveMoved = false; // haveMoved becomes/remains false
     }
 }
 
 // LED on pkm weight
-const writeJSONToArduino = async (propertyName, callback) => {
-    if (!state.serial) throw new Error("No Arduino connected to write the data to!");
+const writeJSONToArduino = async (propertyName, callback) => { // asynchronous writeJSONToArduino method
+    if (!state.serial) throw new Error("No Arduino connected to write the data to!"); // throws error if Arduino not connected
     if (state.writing) return; // it is possible to send double writes, which we do not want!
 
-    state.writing = true;
+    state.writing = true; //sets state.writing to true
     const data = state[propertyName]; // First, we get the object an object and turn it into JSON.
     const json = JSON.stringify(data, null, 0); // Transform our internal JS object into JSON representation, which we store as a string
-
-    // The serial writer will want the data in a specific format, which we can do with the TextEncoder object, see https://developer.mozilla.org/en-US/docs/Web/API/TextEncoder
-    const payload = new TextEncoder().encode(json);
-
-    // Get the writer, write to it and then release it for the next write
-    const writer = await state.serial.writable.getWriter();
-    await writer.write(payload);
-    writer.releaseLock();
-
+    const payload = new TextEncoder().encode(json); // The serial writer will want the data in a specific format, which we can do with the TextEncoder object
+    const writer = await state.serial.writable.getWriter(); // Get the writer
+    await writer.write(payload); // write to it
+    writer.releaseLock(); // then release it for the next write
     if (callback) callback(); // Run the callback function, if any
-    state.writing = false;
+    state.writing = false; // set state.writing to false
 } // <-----
 
-const mapRange = (value, fromLow, fromHigh, toLow, toHigh) => {
-    return toLow + (toHigh - toLow) * (value - fromLow) / (fromHigh - fromLow);
+const mapRange = (value, fromLow, fromHigh, toLow, toHigh) => { // mapRange method (value is pkmn weight)
+    return toLow + (toHigh - toLow) * (value - fromLow) / (fromHigh - fromLow); // Equation for setting brightness based on pokemon weight
 } // <----
 
-const setLEDToPkmWeight = async (weight) => {
-    state.dataToWrite.brightness = mapRange(weight, 0, 1500, 0, 255);
-   
-    console.log(weight)
+const setLEDToPkmWeight = async (weight) => { // setLEDToPkmWeight method that increases brightness the heavier the pmkn
+    state.dataToWrite.brightness = mapRange(weight, 0, 1500, 0, 255); // calls mapRange method
 } // <-----
 
 
-
-
-// fetch pokemons
-
-fetchPokemon();
-
-/*
-next_button.addEventListener("click", () => {
-    if (num < 898) {
-        num++;
-        console.log("Pokemon Number: " + num);
-        fetchPokemon();
-    }
-})
-
-prev_button.addEventListener("click", () => {
-    if (num > 1)
-        num--;
-    console.log("Pokemon Number: " + num);
-    fetchPokemon();
-})
-*/
-
-
-
-
-/*const fetchPokemon = () => {
-    const promises = [];
-    for (let i = 1; i <= 898; i++) {
-        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        promises.push(fetch(url).then((res) => res.json()));
-    }
-    Promise.all(promises).then((results) => {
-        const pokemon = results.map((result) => ({
-            name: result.name,
-            image: result.sprites['front_default'],
-            type: result.types.map((type) => type.type.name).join(', '),
-            id: result.id
-        }));
-        displayPokemon(pokemon);
-    });
-};*/
+fetchPokemon(); // fetch Pokemons
